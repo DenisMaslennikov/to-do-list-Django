1b267eabdc28d88b8ab803e0b5a3e2ece35c0560
Denis Maslennikov <ktotom7@yandex.ru>
2024-11-13 11:32:22 +0300

    remove id from related fields

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -7,15 +7,15 @@ from core.models import UUIDPrimaryKeyMixin
 User = get_user_model()
 
 
-class Task(UUIDPrimaryKeyMixin, models.Model):
+class Task(UUIDPrimaryKeyMixin):
     """Модель задачи."""
 
     title = models.CharField(max_length=100, help_text="Заголовок задачи")
     description = models.TextField(help_text="Описание задачи")
-    task_status_id = models.ForeignKey(
+    task_status = models.ForeignKey(
         TaskStatus, on_delete=models.PROTECT, help_text="Статус задачи", related_name="tasks"
     )
-    user_id = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Пользователь", related_name="tasks")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="Пользователь", related_name="tasks")
     created_at = models.DateTimeField(auto_now_add=True, help_text="Создана")
     updated_at = models.DateTimeField(auto_now=True, help_text="Обновлена")
     complete_before = models.DateTimeField(help_text="Завершить до", null=True, blank=True)
